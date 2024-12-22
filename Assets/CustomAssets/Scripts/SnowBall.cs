c318ac324fcd5dd771b09403e19995dec364458d
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2024-12-21 23:26:57 -0700

    model and foundation for snow ball

---
+++ Assets/CustomAssets/Scripts/SnowBall.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBall : MonoBehaviour
+{
+    // TODO: pick up and break sound effects
+    // TODO: destroy, decal, and particles on collision
+    // TODO: make VR interactable (might be handled in player controls)
+}
