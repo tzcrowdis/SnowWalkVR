6c89490b7e3ed9421c0b8d9d3bd67f1837349553
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2025-01-01 17:24:50 -0700

    Added Bonfire

--- Assets/CustomAssets/Scripts/HandFollowController.cs
+++ Assets/CustomAssets/Scripts/HandFollowController.cs
@@ -10,6 +10,10 @@ public class HandFollowController : MonoBehaviour
     Rigidbody rb;
     Transform target;
 
+    Transform center;
+    bool follow; // NOTE added to let picking up snowballs work
+    // NOTE not working!!!!
+
     private void Start()
     {
         if (gameObject.name.Contains("Right"))
@@ -17,18 +21,42 @@ public class HandFollowController : MonoBehaviour
         else if (gameObject.name.Contains("Left"))
             target = GameObject.Find("Left Hand Position").transform;
 
+        center = transform;
+        follow = false;
+
         rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!collision.gameObject.name.Contains("SnowBall"))
+        {
+            follow = true;
+            rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        follow = false;
+        transform.position = center.position;
+        transform.rotation = center.rotation;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 
     void FixedUpdate()
     {
-        // track position
-        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        if (follow)
+        {
+            // track position
+            rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
 
-        // track rotation
-        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
-        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
-        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
-        rb.angularVelocity = rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime;
+            // track rotation
+            Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+            rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+            Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+            rb.angularVelocity = rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime;
+        }
     }
 }
