d0a722807ff1553b9f1e50876dd0a1cffb7451bf
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2025-01-07 18:04:47 -0700

    Ghost Bride AI and Bonfire Script

--- Assets/CustomAssets/Scripts/GhostBrideBehavior.cs
+++ Assets/CustomAssets/Scripts/GhostBrideBehavior.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class GhostBrideBehavior : MonoBehaviour
 {
-    NavMeshAgent agent;
-    public Vector3 destination;
+    [HideInInspector]
+    public NavMeshAgent agent;
+    public GameObject bonfire;
+    public bool chanting;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        // TODO enable walking animation
+
+        chanting = false;
     }
 
     void Update()
     {
-        agent.destination = destination;
+        transform.LookAt(agent.destination);
+
+        // reached location -> look at fire and start chanting
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+            {
+                transform.LookAt(bonfire.transform);
+                // TODO change animation
+
+                chanting = true;
+            }
+        }
     }
 }
